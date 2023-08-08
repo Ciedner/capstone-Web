02bf88d219ff7f98ac3b0212bc2faf64c0d7fbc3
Ciedner <ciednermabale@gmail.com>
2023-08-08 20:33:21 +0800

    feedbacks

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { fetchAllFeedback, fetchFeedbackById } from './FeedbackAPI.js';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+import { FaUserCircle } from "react-icons/fa";
+import { useLocation, Link } from "react-router-dom";
 
 const FeedbackPage = () => {
   const [feedbackList, setFeedbackList] = useState([]);
@@ -32,6 +35,22 @@ const FeedbackPage = () => {
       });
   };
 
+  const handleDeleteFeedback = (id) => {
+    const confirmed = window.confirm('Are you sure you want to delete this feedback?');
+    if (confirmed) {
+      const updatedFeedbackList = feedbackList.filter((feedback) => feedback.id !== id);
+      setFeedbackList(updatedFeedbackList);
+
+      const updatedClickedIds = clickedFeedbackIds.filter((clickedId) => clickedId !== id);
+      localStorage.setItem('clickedFeedbackIds', JSON.stringify(updatedClickedIds));
+      setClickedFeedbackIds(updatedClickedIds);
+
+      if (selectedFeedback && selectedFeedback.id === id) {
+        setSelectedFeedback(null);
+      }
+    }
+  };
+
   useEffect(() => {
     const clickedIds = JSON.parse(localStorage.getItem('clickedFeedbackIds')) || [];
     setClickedFeedbackIds(clickedIds);
@@ -62,6 +81,7 @@ const FeedbackPage = () => {
     fontSize: '18px',
     fontWeight: 'bold',
     fontFamily: 'Georgina',
+    marginLeft: '50px'
   };
 
   const styles = {
@@ -73,8 +93,20 @@ const FeedbackPage = () => {
       backgroundColor: '#fff',
       borderRadius: '10px',
       boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-      marginTop:'80px',
+      marginTop: '70px'
     },
+      welcomeMessage: {
+        position: "absolute",
+        top: "10px",
+        right: "10px",
+        margin: "0",
+        color: "#fff",
+        fontFamily: "Rockwell, sans-serif",
+        textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+      },
+      icon: {
+        marginRight: "5px",
+      },
     feedbackContainer: {
       padding: '20px',
       backgroundColor: '#f9f9f9',
@@ -89,22 +121,44 @@ const FeedbackPage = () => {
     marginTop: '10px',
   };
 
+
   return (
+    <div>
+    <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "#003851", marginBottom: '20px' }}>
+    <div className="container">
+      <Link className="navbar-brand" to="/Dashboard">
+        SpotWise Parking Management System
+      </Link>
+      <p style={styles.welcomeMessage}>
+      <DropdownButton 
+            alignRight
+            variant="outline-light"
+            title={<FaUserCircle style={styles.icon} />}
+            id="dropdown-menu"
+          >
+            <Dropdown.Item href="/Dashboard">Dashboard</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item href="/">Logout</Dropdown.Item>
+          </DropdownButton>
+      </p>
+    </div>
+  </nav>
     <div style={styles.mainContainer}>
       <h1 style={{ textAlign: 'center', fontFamily: 'Georgina', fontSize: '32px' }}>
-        Customer Feedback Management
-      </h1>
+        Customer Feedback
+              </h1>
       <div style={{ display: 'flex' }}>
         <div style={{ ...styles.feedbackContainer, flex: 1 }}>
           <div style={navbarStyle}>
-            <div style={logoStyle}> FEEDBACK LIST </div>
+            <div style={logoStyle}> 
+           FEEDBACK LIST</div>
           </div>
           <ul>
             {pageFeedbackList.map((feedback) => {
               const isClicked = clickedFeedbackIds.includes(feedback.id);
               const listItemStyle = {
                 ...feedbackListItemStyle,
-                backgroundColor: isClicked ? '#f0f0f0' : 'transparent', // Highlight style
+                backgroundColor: isClicked ? '#f0f0f0' : 'transparent', 
               };
 
               return (
@@ -131,7 +185,7 @@ const FeedbackPage = () => {
             </button>
           </div>
         </div>
-        <div style={{ ...styles.feedbackContainer, flex: 1, marginLeft: '20px' }}> {/* Right container */}
+        <div style={{ ...styles.feedbackContainer, flex: 1, marginLeft: '20px' }}> 
           {selectedFeedback ? (
             <div>
               <div style={navbarStyle}>
@@ -149,6 +203,7 @@ const FeedbackPage = () => {
           )}
         </div>
       </div>
+    </div>
     </div>
   );
 };
