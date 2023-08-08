2b1c96f1cfa6fe85ae0915d29db7e91c2c007a63
Ciedner <ciednermabale@gmail.com>
2023-08-08 16:31:38 +0800

    feedback again

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,12 +1,12 @@
-// FeedbackPage.js
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { fetchAllFeedback, fetchFeedbackById } from "./FeedbackAPI.js";
-import { genHoverStyle } from "antd/es/input/style/index.js";
+import React, { useState, useEffect } from 'react';
+import { fetchAllFeedback, fetchFeedbackById } from './FeedbackAPI.js';
 
 const FeedbackPage = () => {
   const [feedbackList, setFeedbackList] = useState([]);
   const [selectedFeedback, setSelectedFeedback] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [clickedFeedbackIds, setClickedFeedbackIds] = useState([]);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     fetchAllFeedback()
@@ -14,7 +14,7 @@ const FeedbackPage = () => {
         setFeedbackList(data);
       })
       .catch((error) => {
-        console.error("Error fetching feedback:", error);
+        console.error('Error fetching feedback:', error);
       });
   }, []);
 
@@ -22,71 +22,127 @@ const FeedbackPage = () => {
     fetchFeedbackById(id)
       .then((data) => {
         setSelectedFeedback(data);
+        const clickedIds = JSON.parse(localStorage.getItem('clickedFeedbackIds')) || [];
+        const updatedClickedIds = [...clickedIds, id];
+        localStorage.setItem('clickedFeedbackIds', JSON.stringify(updatedClickedIds));
+        setClickedFeedbackIds(updatedClickedIds);
       })
       .catch((error) => {
-        console.error("Error fetching feedback:", error);
+        console.error('Error fetching feedback:', error);
       });
   };
 
-  const feedbackPageStyle = {
-    fontFamily: "Arial, sans-serif",
-    padding: "20px",
-  };
+  useEffect(() => {
+    const clickedIds = JSON.parse(localStorage.getItem('clickedFeedbackIds')) || [];
+    setClickedFeedbackIds(clickedIds);
+  }, []);
 
-  const flexContainerStyle = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-  };
-
-  const feedbackListStyle = {
-    flex: "1",
-    paddingRight: "20px",
-    borderRight: "1px solid #ccc",
-  };
-
-  const feedbackDetailsStyle = {
-    flex: "2",
-    paddingLeft: "100px",
-  };
+  const totalPages = Math.ceil(feedbackList.length / itemsPerPage);
+  const pageFeedbackList = feedbackList.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 
   const feedbackListItemStyle = {
-    cursor: "pointer",
-    padding: "5px 0",
-    borderBottom: "1px solid #ccc",
+    cursor: 'pointer',
+    padding: '5px 0',
+    borderBottom: '1px solid #ccc',
+    fontFamily: 'Georgina',
+    marginTop: '10px',
   };
 
-  const feedbackListItemHoverStyle = {
-    backgroundColor: "#f0f0f0",
+  const navbarStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '20px',
+    backgroundColor: 'black',
+    color: 'white',
+    width: '100%',
+  };
+
+  const logoStyle = {
+    fontSize: '18px',
+    fontWeight: 'bold',
+    fontFamily: 'Georgina',
+  };
+
+  const styles = {
+    mainContainer: {
+      marginTop: '50px',
+      maxWidth: '800px',
+      margin: '0 auto',
+      padding: '20px',
+      backgroundColor: '#fff',
+      borderRadius: '10px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+      marginTop:'80px',
+    },
+    feedbackContainer: {
+      padding: '20px',
+      backgroundColor: '#f9f9f9',
+      borderRadius: '5px',
+      border: '1px solid #ccc',
+      marginTop: '30px',
+    },
+  };
+
+  const para = {
+    fontFamily: 'Georgina',
+    marginTop: '10px',
   };
 
   return (
-    <div style={feedbackPageStyle}>
-      <h1>Customer Feedback Management</h1>
-      <div style={flexContainerStyle}>
-        <div style={feedbackListStyle}>
-          <h2>Feedback List</h2>
+    <div style={styles.mainContainer}>
+      <h1 style={{ textAlign: 'center', fontFamily: 'Georgina', fontSize: '32px' }}>
+        Customer Feedback Management
+      </h1>
+      <div style={{ display: 'flex' }}>
+        <div style={{ ...styles.feedbackContainer, flex: 1 }}>
+          <div style={navbarStyle}>
+            <div style={logoStyle}> FEEDBACK LIST </div>
+          </div>
           <ul>
-            {feedbackList.map((feedback) => (
-              <li
-                key={feedback.id}
-                onClick={() => handleFeedbackClick(feedback.id)}
-                style={feedbackListItemStyle} 
-              >
-                {feedback.customerName}
-              </li>
-            ))}
+            {pageFeedbackList.map((feedback) => {
+              const isClicked = clickedFeedbackIds.includes(feedback.id);
+              const listItemStyle = {
+                ...feedbackListItemStyle,
+                backgroundColor: isClicked ? '#f0f0f0' : 'transparent', // Highlight style
+              };
+
+              return (
+                <li
+                  key={feedback.id}
+                  onClick={() => handleFeedbackClick(feedback.id)}
+                  style={listItemStyle}
+                >
+                  {feedback.customerName}
+                </li>
+              );
+            })}
           </ul>
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+              Previous Page
+            </button>
+            <span style={{ margin: '0 10px' }}>Page {currentPage}</span>
+            <button
+              onClick={() => setCurrentPage(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              Next Page
+            </button>
+          </div>
         </div>
-        <div style={feedbackDetailsStyle}>
+        <div style={{ ...styles.feedbackContainer, flex: 1, marginLeft: '20px' }}> {/* Right container */}
           {selectedFeedback ? (
             <div>
-              <h2>Feedback Details</h2>
-              <p>Customer Name: {selectedFeedback.customerName}</p>
-              <p>Email: {selectedFeedback.email}</p>
-              <p>Feedback Type: {selectedFeedback.feedbackType}</p>
-              <p>Rating: {selectedFeedback.rating}</p>
-              <p>Content: {selectedFeedback.content}</p>
+              <div style={navbarStyle}>
+                <div style={logoStyle}> FEEDBACK DETAILS </div>
+              </div>
+              <div style={para}>
+                <p>Customer Name: {selectedFeedback.customerName}</p>
+                <p>Email: {selectedFeedback.email}</p>
+                <p>Feedback Type: {selectedFeedback.feedbackType}</p>
+                <p>Content: {selectedFeedback.content}</p>
+              </div>
             </div>
           ) : (
             <p>Select a feedback entry to view details.</p>
--- src/components/FeedbackAPI.js
+++ src/components/FeedbackAPI.js
@@ -1,29 +1,74 @@
-// FeedbackAPI.js
 const feedbackData = [
     {
       id: 1,
-      customerName: "John Doe",
-      email: "john@example.com",
+      customerName: "Junas Gutib",
+      email: "junas@example.com",
       feedbackType: "Suggestion",
-      content: "Great service, but could use more variety.",
-      rating: 4,
+      content: "Wait time was too long. Need to be fixed",
     },
     {
       id: 2,
-      customerName: "Jane Smith",
+      customerName: "Jason Gilbert",
+      email: "gilbert@example.com",
+      feedbackType: "Complaint",
+      content: "The operator was not in the mood.",
+    },
+    {
+      id: 3,
+      customerName: "Jason Patigayon",
       email: "jane@example.com",
       feedbackType: "Complaint",
-      content: "Wait time was too long.",
-      rating: 2,
+      content: "Parking area is tight.",
     },
-    // Add more feedback entries here...
+    {
+      id: 4,
+      customerName: "Hans Bernard",
+      email: "hans@example.com",
+      feedbackType: "Suggestion",
+      content: "Need to have a wider parking area",
+    },
+    {
+      id: 5,
+      customerName: "James Bulfa",
+      email: "james@example.com",
+      feedbackType: "Complaint",
+      content: "Wait time was too long.",
+    },
+    {
+      id: 6,
+      customerName: "Marky Rocaberte",
+      email: "marky@example.com",
+      feedbackType: "Complaint",
+      content: "Wait time was too long.",
+    },
+    {
+      id: 7,
+      customerName: "Ian Alonzo",
+      email: "ian@example.com",
+      feedbackType: "Complaint",
+      content: "Prize is not recommendable for this kind of parking space.",
+    },
+    {
+      id: 8,
+      customerName: "Maybell Cardenas ",
+      email: "maybell@example.com",
+      feedbackType: "Complaint",
+      content: "Too tight",
+    },
+    {
+      id: 9,
+      customerName: "Daisy Ree",
+      email: "ree@example.com",
+      feedbackType: "Complaint",
+      content: "The space is too tight",
+    },
   ];
   
   export function fetchAllFeedback() {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve(feedbackData);
-      }, 500); // Simulating API delay
+      }, 500); 
     });
   }
   
@@ -47,7 +92,7 @@ const feedbackData = [
         const feedbackWithId = { ...newFeedback, id: newId };
         feedbackData.push(feedbackWithId);
         resolve(newId);
-      }, 500); // Simulating API delay
+      }, 500);
     });
   }
   
