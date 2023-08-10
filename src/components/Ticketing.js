15e56ede35eb4be6c0af3160b37027b32db72338
Ciedner <ciednermabale@gmail.com>
2023-08-10 14:40:01 +0800

    ticket with available spaces

--- src/components/Ticketing.js
+++ src/components/Ticketing.js
@@ -3,14 +3,23 @@ import { Button } from 'react-bootstrap';
 
 const ParkingSlot = () => {
   const maxZones = 5;
-  const [slotSets, setSlotSets] = useState([{ title: 'Zone 1', slots: Array(15).fill(false) }]);
+  const initialSlotSets = [{ title: 'Zone 1', slots: Array(15).fill(false) }];
+  const initialTotalSpaces = initialSlotSets[0].slots.length * initialSlotSets.length;
+
+  const [slotSets, setSlotSets] = useState(initialSlotSets);
   const [currentSetIndex, setCurrentSetIndex] = useState(0);
+  const [availableSpaces, setAvailableSpaces] = useState(initialTotalSpaces);
 
   const toggleOccupancy = (setIndex, boxIndex) => {
     const updatedSets = [...slotSets];
     updatedSets[setIndex].slots[boxIndex] = !updatedSets[setIndex].slots[boxIndex];
     setSlotSets(updatedSets);
+  
+    const occupiedSpaces = updatedSets.reduce((total, set) => total + set.slots.filter(slot => slot).length, 0);
+    const updatedAvailableSpaces = initialTotalSpaces - occupiedSpaces;
+    setAvailableSpaces(updatedAvailableSpaces);
   };
+  
 
   const rows = 5;
   const cols = 3;
@@ -18,7 +27,7 @@ const ParkingSlot = () => {
   const handleNext = () => {
     if (currentSetIndex === slotSets.length - 1) {
       if (slotSets.length < maxZones) {
-        setSlotSets((prevSets) => [
+        setSlotSets(prevSets => [
           ...prevSets,
           { title: `Zone ${prevSets.length + 1}`, slots: Array(15).fill(false) },
         ]);
@@ -38,7 +47,7 @@ const ParkingSlot = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Parking Lot</h1>
-      <div 
+      <div
         style={{
           display: 'grid',
           gridTemplateRows: `auto repeat(${rows}, 1fr)`,
@@ -51,7 +60,7 @@ const ParkingSlot = () => {
           marginTop: '50px',
         }}
       >
-        <div style={{ gridColumn: '1 / -1', textAlign:'center', fontSize: '20px', fontWeight: 'bold', marginBottom:'20px' }}>
+        <div style={{ gridColumn: '1 / -1', textAlign: 'center', fontSize: '20px', fontWeight: 'bold', marginBottom: '20px' }}>
           {slotSets[currentSetIndex].title}
         </div>
         {slotSets[currentSetIndex].slots.map((isOccupied, index) => (
@@ -76,6 +85,17 @@ const ParkingSlot = () => {
         <div style={{ gridColumn: '1 / -1', textAlign: 'right', fontSize: '20px', fontWeight: 'bold', marginTop: '20px', marginRight: '50px' }}>
           <Button onClick={handlePrev} style={{ marginRight: '20px', backgroundColor: 'gray' }} disabled={currentSetIndex === 0}>Prev</Button>
           <Button onClick={handleNext} disabled={slotSets.length >= maxZones}>Next</Button>
+        </div>
+        <div style={{ gridColumn: '1 / -1', textAlign: 'center', marginTop: '20px' }}>
+          <div style={{ display: 'inline-block', width: '20px', height: '20px', backgroundColor: 'green', marginRight: '10px' }}></div>
+          <span>Available</span>
+          <div style={{ display: 'inline-block', width: '20px', height: '20px', backgroundColor: 'red', marginLeft: '20px', marginRight: '10px' }}></div>
+          <span>Occupied</span>
+        </div>
+        <div style={{ gridColumn: '1 / -1', textAlign: 'center', marginTop: '20px' }}>
+          <span>Total Parking Spaces: {initialTotalSpaces}</span>
+          <br />
+          <span>Available Spaces: {availableSpaces}</span>
         </div>
       </div>
     </div>
