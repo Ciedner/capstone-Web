9d121ea8e8fe004e855b49a1de72ae736ddfd7ed
Ciedner <ciednermabale@gmail.com>
2023-12-28 18:44:50 +0800

    reservation update

--- src/Operator/Reservation.js
+++ src/Operator/Reservation.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, query, where, getDocs, doc, getDoc } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
 import { db, auth } from "../config/firebase";
 import UserContext from '../UserContext';
@@ -9,32 +9,58 @@ const Reservation = () => {
   const [reservationRequests, setReservationRequests] = useState([]);
   const [historyLog, setHistoryLog] = useState([]);
   const [selectedReservation, setSelectedReservation] = useState(null);
+  const [userNames, setUserNames] = useState({});
+
 
   const fetchReservations = async (managementName) => {
+    console.log("Fetching reservations for managementName:", managementName);
     const q = query(collection(db, 'reservations'), where('managementName', '==', managementName));
     try {
       const querySnapshot = await getDocs(q);
-      const reservations = querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        name: doc.data().name, 
-        plateNumber: doc.data().carPlate,
-        floor: doc.data().slotId.charAt(0), 
-        slot: doc.data().slotId.slice(1), 
-        timeOfRequest: new Date(doc.data().timestamp.seconds * 1000).toLocaleTimeString('en-US', { hour12: true, hour: 'numeric', minute: 'numeric' })
-      }));
+      const reservationPromises = querySnapshot.docs.map(async (reservationDoc) => {
+        const slotId = reservationDoc.data().slotId;
+        const userEmail = reservationDoc.data().userEmail;
+  
+        // Fetch the floor name from the slotData sub-document
+        const slotDocRef = doc(db, 'slot', managementName, 'slotData', `slot_${slotId}`);
+        const slotDocSnapshot = await getDoc(slotDocRef);
+  
+        // Fetch user data
+        const userQuery = query(collection(db, 'user'), where('email', '==', userEmail));
+        const userSnapshot = await getDocs(userQuery);
+        const userData = userSnapshot.docs[0]?.data();
+
+        setUserNames((prevUserNames) => ({
+          ...prevUserNames,
+          [userEmail]: userData?.name || 'N/A',
+        }));
+  
+        return {
+          id: reservationDoc.id,
+          name: reservationDoc.data().name,
+          userName: userData?.name || 'N/A',  // Add the userName property
+          plateNumber: userData?.carPlateNumber || 'N/A',
+          slot: typeof slotId === 'string' ? slotId.slice(1) : 'N/A',
+          slotId: slotId,
+          timeOfRequest: new Date(reservationDoc.data().timestamp.seconds * 1000).toLocaleTimeString('en-US', { hour12: true, hour: 'numeric', minute: 'numeric' })
+        };
+      });
+      const reservations = await Promise.all(reservationPromises);
+      console.log("Fetched reservations:", reservations);
       setReservationRequests(reservations);
     } catch (error) {
-      console.error("Error fetching reservations: ", error);
+      console.error("Error fetching reservations:", error);
     }
   };
-
+  
+  
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, currentUser => {
       if (currentUser && user?.managementName) {
-        
+        console.log("User authenticated. Fetching reservations...");
         fetchReservations(user.managementName);
       } else {
-
+        console.log("User not authenticated or managementName is null.");
         setReservationRequests([]);
       }
     });
@@ -55,14 +81,13 @@ const Reservation = () => {
       setHistoryLog(storedHistoryLog);
     }
   }, []);
-  const handleReservation = (accepted, name, plateNumber, floor, slot, timeOfRequest, index) => {
+  const handleReservation = (accepted, name, plateNumber, slot, timeOfRequest, index) => {
     const status = accepted ? 'Accepted' : 'Declined';
 
     const logEntry = {
       status,
       name,
       plateNumber,
-      floor,
       slot,
       timeOfRequest,
     };
@@ -79,7 +104,6 @@ const Reservation = () => {
       status,
       name,
       plateNumber,
-      floor,
       slot,
       timeOfRequest,
     });
@@ -89,7 +113,7 @@ const Reservation = () => {
     <div className="history-log mt-4" style={{ maxHeight: '200px', overflowY: 'scroll' }}>
       {historyLog.map((logEntry, index) => (
         <div className={`alert ${logEntry.status === 'Accepted' ? 'alert-success' : 'alert-danger'} mt-2`} key={index}>
-          <strong>{logEntry.status}:</strong> {logEntry.name} requested a reservation on {logEntry.timeOfRequest}. Plate Number: {logEntry.plateNumber}, Floor: {logEntry.floor}, Slot: {logEntry.slot}
+          <strong>{logEntry.status}:</strong> {logEntry.name} requested a reservation on {logEntry.timeOfRequest}. Plate Number: {logEntry.plateNumber}, Slot: {logEntry.slot}
         </div>
       ))}
     </div>
@@ -97,17 +121,17 @@ const Reservation = () => {
 
   const ReservationRequest = ({ request, index }) => (
     <div className="reservation-request mb-4 border p-3 rounded bg-light" key={request.plateNumber}>
-      <h4 className="mb-0">Name: {request.name}</h4>
+      <h4 className="mb-0">Name: {request.userName}</h4>
       <p className="text-muted mb-2">Time of Request: {request.timeOfRequest}</p>
       <p>Plate Number: {request.plateNumber}</p>
-      <p>Floor Number: {request.floor}</p>
-      <p>Slot Number: {request.slot}</p>
+      <p>Slot Number: {request.slotId}</p>
       <div className="d-flex flex-column align-items-center mt-2">
-        <button className="btn btn-success" onClick={() => handleReservation(true, request.name, request.plateNumber, request.floor, request.slot, request.timeOfRequest, index)}>Accept Reservation</button>
-        <button className="btn btn-danger mt-2" onClick={() => handleReservation(false, request.name, request.plateNumber, request.floor, request.slot, request.timeOfRequest, index)}>Decline Reservation</button>
+        <button className="btn btn-success" onClick={() => handleReservation(true, request.userName, request.plateNumber, request.floor, request.slot, request.timeOfRequest, index)}>Accept Reservation</button>
+        <button className="btn btn-danger mt-2" onClick={() => handleReservation(false, request.userName, request.plateNumber, request.floor, request.slot, request.timeOfRequest, index)}>Decline Reservation</button>
       </div>
     </div>
   );
+  
 
   return (
     <div>
@@ -125,7 +149,7 @@ const Reservation = () => {
             <p>No reservation</p>
           ) : (
             reservationRequests.map((request, index) => (
-              <ReservationRequest request={request} index={index} key={request.plateNumber} />
+              <ReservationRequest request={request} index={index} key={index} />
             ))
           )}
         </div>
